5a57fa9c0acfe4c5ddb8fbeb8a3bbe2cb8b129bd
shawntao1011 <shawntao1011@gmail.com>
2020-10-13 12:08:42 -0400

    realized article view function

--- route/admin/article.js
+++ route/admin/article.js
@@ -1,4 +1,13 @@
-module.exports=(req,res)=>{
+const {Article}=require('../../model/article');
+
+module.exports=async (req,res)=>{
     req.app.locals.currentlink='article';
-    res.render('admin/article');
+
+    let articles=await Article.find({});
+
+    //res.send(articles);
+
+    res.render('admin/article',{
+        articles:articles
+    });
 }
